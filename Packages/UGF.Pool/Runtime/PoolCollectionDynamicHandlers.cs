98276834bc4870434e2ca4cd437ff9e11f2d0b49
Vorobey <vorobyovvorobyov@gmail.com>
2022-05-13 21:08:21 +0200

    Add PoolCollectionDynamicHandlers

---
+++ Packages/UGF.Pool/Runtime/PoolCollectionDynamicHandlers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UGF.RuntimeTools.Runtime.Contexts;
+
+namespace UGF.Pool.Runtime
+{
+    public class PoolCollectionDynamicHandlers<TItem> : PoolCollectionDynamic<TItem> where TItem : class
+    {
+        public PoolItemBuildHandler<TItem> BuildHandler { get; }
+        public PoolItemDestroyHandler<TItem> DestroyHandler { get; }
+
+        public PoolCollectionDynamicHandlers(IContext context, PoolItemBuildHandler<TItem> buildHandler, PoolItemDestroyHandler<TItem> destroyHandler, int capacity = 4) : base(context, capacity)
+        {
+            BuildHandler = buildHandler ?? throw new ArgumentNullException(nameof(buildHandler));
+            DestroyHandler = destroyHandler ?? throw new ArgumentNullException(nameof(destroyHandler));
+        }
+
+        protected override TItem OnBuild()
+        {
+            return BuildHandler.Invoke(Context);
+        }
+
+        protected override void OnDestroy(TItem item)
+        {
+            DestroyHandler.Invoke(item, Context);
+        }
+    }
+}
